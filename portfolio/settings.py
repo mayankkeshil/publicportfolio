373527012ba1e8fa4737cd36963e1bc7ec385f12
mayank <mkeshil@protonmail.com>
2025-09-02 16:04:07 +0100

    Edited settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 '''
 import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,15 +79,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Perfumepost19@',
-        'HOST': 'db.tpbmpejdeiphyhkedgia.supabase.co',
-        'PORT': '5432',
-        'OPTIONS': { 'sslmode': 'require' } 
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
+        'OPTIONS': {'sslmode': 'require',}
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
