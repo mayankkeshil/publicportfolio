611aee7aa7abdaabc61fe8da72a800fc3fbf33ff
mayankkeshil <mkeshil@protonmail.com>
2025-12-06 18:37:17 +0000

    S3 keys

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -155,11 +155,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 AWS_S3_ENDPOINT_URL = f'{SUPABASE_URL}/storage/v1/s3'
-AWS_ACCESS_KEY_ID = "supabase"
-AWS_SECRET_ACCESS_KEY = SUPABASE_KEY
+AWS_ACCESS_KEY_ID = os.getenv("SUPABASE_S3_ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = os.getenv("SUPABASE_S3_SECRET_KEY")
 AWS_STORAGE_BUCKET_NAME = "media"
-AWS_S3_REGION_NAME = "us-east-1"
+AWS_S3_REGION_NAME = "auto"
 AWS_S3_ADDRESSING_STYLE = "path"
+
 
 
 MEDIA_URL = f'{SUPABASE_URL}/storage/v1/object/public/media/'
