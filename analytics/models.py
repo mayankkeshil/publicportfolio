98d7fcdbaa452d799d63806360be5a135cba7fe5
mayankkeshil <mkeshil@protonmail.com>
2025-12-22 22:06:29 +0000

    Saved analytics models

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    EVENT_TYPES = [
+        ("page_view", "Page View"),
+        ("click", "Click")
+    ] 
+# Same as
+#allowed_values = ["page_view", "click"]
+#labels = {
+#    "page_view": "Page View",
+#    "click": "Click"
+#}
+
+    event_type = models.CharField(max_length=20, choices=EVENT_TYPES)
+    page = models.CharField(max_length=200)
+    label = models.CharField(max_length=200, null=True, blank=True) # whats label
+    session_id = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.event_type} - {self.page}"
--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Event
 
-# Create your views here.
+print("LOADED ANALYTICS VIEWS")
+
+@csrf_exempt
+def track_event(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        Event.objects.create(
+            event_type=data["event_type"],
+            page=data.get("page", ""),
+            label=data.get("label"),
+            session_id=request.session.session_key or "anon"
+        )
+
+        return JsonResponse({"status": "ok"})
+
+    return JsonResponse({"error": "POST only"}, status=400)
+
+def track_event(request):
+    print("TRACK EVENT CALLED")
+    return None
